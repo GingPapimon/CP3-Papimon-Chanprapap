ef4a6c91a4cd77b9aec87c759d78a2bd52ad78f3
GingPapimon <142671656+GingPapimon@users.noreply.github.com>
2023-08-19 21:19:10 +0700

    Add files via upload

---
+++ Exercise4_Papimon_C.py
@@ -0,0 +1,9 @@
+scoreEng = 75.0
+scoreBus = 80.0
+scoreCom = 65.0
+scoreProg = 85.0
+print("------------- Your Score -------------")
+print("Foundation English               :",scoreEng)
+print("General Business                 :",scoreBus)
+print("Introduction to Computer Systems :",scoreCom)
+print("Computer Programming             :",scoreProg)
