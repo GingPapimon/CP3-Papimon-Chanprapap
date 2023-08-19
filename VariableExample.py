0443389e79b529bba779d28623297505b3f468cc
GingPapimon <142671656+GingPapimon@users.noreply.github.com>
2023-08-19 20:28:13 +0700

    Add files via upload

---
+++ VariableExample.py
@@ -0,0 +1,13 @@
+x = 10
+print(x)
+y = 5
+print(y)
+print("Homework")
+Name = "Papimon Chanprapap"
+Age = "23"
+Height = "160"
+Weight = "53"
+print("Name   :",Name)
+print("Age    :",Age, "years")
+print("Height :",Height, "cm")
+print("Weight :",Weight, "kg")
